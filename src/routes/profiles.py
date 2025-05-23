a980bef6d6d678eef9857cf427f9f6321bedff38
rompu <rom.puh@gmail.com>
2025-05-23 12:55:59 +0300

    Solution

--- src/routes/profiles.py
+++ src/routes/profiles.py
@@ -1,5 +1,133 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status, Depends, HTTPException
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from config import get_jwt_auth_manager, get_s3_storage_client
+from database import (
+    get_db, UserGroupModel, UserModel, UserProfileModel, UserGroupEnum,
+)
+from exceptions import BaseSecurityError, S3FileUploadError
+
+from schemas import ProfileRequestSchema, ProfileResponseSchema
+from security.http import get_token
+from security.interfaces import JWTAuthManagerInterface
+from storages import S3StorageInterface
+from validation import validate_image
 
 router = APIRouter()
 
-# Write your code here
+
+@router.post(
+    "/users/{user_id}/profile/",
+    response_model=ProfileResponseSchema,
+    summary="User profile creation",
+    description="User profile creation",
+    status_code=status.HTTP_201_CREATED,
+)
+async def create_profile(
+        user_id: int,
+        request: ProfileRequestSchema = Depends(
+            ProfileRequestSchema.from_form
+        ),
+        token: str = Depends(get_token),
+        db: AsyncSession = Depends(get_db),
+        jwt_manager: JWTAuthManagerInterface = Depends(get_jwt_auth_manager),
+        s3_client: S3StorageInterface = Depends(get_s3_storage_client),
+):
+    try:
+        payload = jwt_manager.decode_access_token(token)
+        token_user_id = payload.get("user_id")
+    except BaseSecurityError as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=str(e),
+        )
+
+    try:
+        validate_image(request.avatar)
+    except ValueError as e:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=[{
+                "type": "value_error",
+                "loc": ["avatar"],
+                "msg": str(e),
+                "input": request.avatar.filename
+            }]
+        )
+
+    if user_id != token_user_id:
+        stmt = (
+            select(UserGroupModel)
+            .join(UserModel)
+            .where(UserModel.id == token_user_id)
+        )
+        result = await db.execute(stmt)
+        user_group = result.scalars().first()
+        if not user_group or user_group.name == UserGroupEnum.USER:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="You don't have permission to edit this profile."
+            )
+
+    stmt = select(UserModel).where(UserModel.id == user_id)
+    result = await db.execute(stmt)
+    user = result.scalars().first()
+    if not user or user.is_active is False:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User not found or not active."
+        )
+
+    stmt_profile = (select(UserProfileModel)
+                    .where(UserProfileModel.user_id == user.id))
+    result_profile = await db.execute(stmt_profile)
+    existing_profile = result_profile.scalars().first()
+    if existing_profile:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User already has a profile."
+        )
+
+    avatar_bytes = await request.avatar.read()
+    avatar_key = f"avatars/{user_id}_{request.avatar.filename}"
+
+    try:
+        await s3_client.upload_file(
+            file_name=avatar_key,
+            file_data=avatar_bytes,
+        )
+    except S3FileUploadError as e:
+        print(f"Error uploading avatar to S3: {e}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Failed to upload avatar. Please try again later."
+        )
+
+    new_profile = UserProfileModel(
+        user_id=user.id,
+        first_name=request.first_name,
+        last_name=request.last_name,
+        gender=request.gender,
+        date_of_birth=request.date_of_birth,
+        info=request.info,
+        avatar=avatar_key,
+    )
+
+    db.add(new_profile)
+    await db.commit()
+    await db.refresh(new_profile)
+
+    avatar_url = await s3_client.get_file_url(new_profile.avatar)
+
+    return ProfileResponseSchema(
+        id=new_profile.id,
+        user_id=new_profile.user_id,
+        first_name=new_profile.first_name,
+        last_name=new_profile.last_name,
+        gender=new_profile.gender,
+        date_of_birth=new_profile.date_of_birth,
+        info=new_profile.info,
+        avatar=avatar_url,
+
+    )
--- src/schemas/profiles.py
+++ src/schemas/profiles.py
@@ -2,6 +2,7 @@ from datetime import date
 
 from fastapi import UploadFile, Form, File, HTTPException
 from pydantic import BaseModel, field_validator, HttpUrl
+from pydantic_core.core_schema import FieldValidationInfo
 
 from validation import (
     validate_name,
@@ -10,4 +11,128 @@ from validation import (
     validate_birth_date
 )
 
-# Write your code here
+
+class ProfileRequestSchema(BaseModel):
+    first_name: str
+    last_name: str
+    gender: str
+    date_of_birth: date
+    info: str
+    avatar: UploadFile
+
+    model_config = {
+        "from_attributes": True
+    }
+
+    @classmethod
+    def from_form(
+            cls,
+            first_name: str = Form(...),
+            last_name: str = Form(...),
+            gender: str = Form(...),
+            date_of_birth: date = Form(...),
+            info: str = Form(...),
+            avatar: UploadFile = File(...)
+    ):
+        return cls(
+            first_name=first_name,
+            last_name=last_name,
+            gender=gender,
+            date_of_birth=date_of_birth,
+            info=info,
+            avatar=avatar
+        )
+
+    @field_validator("first_name", "last_name")
+    @classmethod
+    def validate_name_field(cls, name: str, info: FieldValidationInfo) -> str:
+        try:
+            validate_name(name)
+            return name.lower()
+        except ValueError as e:
+            raise HTTPException(
+                status_code=422,
+                detail=[{
+                    "type": "value_error",
+                    "loc": [info.field_name],
+                    "msg": str(e),
+                    "input": name
+                }]
+            )
+
+    @field_validator("avatar")
+    @classmethod
+    def validate_image_field(cls, avatar: UploadFile) -> UploadFile:
+        try:
+            validate_image(avatar)
+            return avatar
+        except ValueError as e:
+            raise HTTPException(
+                status_code=422,
+                detail=[{
+                    "type": "value_error",
+                    "loc": ["avatar"],
+                    "msg": str(e),
+                    "input": avatar.filename
+                }]
+            )
+
+    @field_validator("gender")
+    @classmethod
+    def validate_gender_field(cls, gender: str) -> str:
+        try:
+            validate_gender(gender)
+            return gender
+        except ValueError as e:
+            raise HTTPException(
+                status_code=422,
+                detail=[{
+                    "type": "value_error",
+                    "loc": ["gender"],
+                    "msg": str(e),
+                    "input": gender
+                }]
+            )
+
+    @field_validator("date_of_birth")
+    @classmethod
+    def validate_birth_date_field(cls, date_of_birth: date) -> date:
+        try:
+            validate_birth_date(date_of_birth)
+            return date_of_birth
+        except ValueError as e:
+            raise HTTPException(
+                status_code=422,
+                detail=[{
+                    "type": "value_error",
+                    "loc": ["date_of_birth"],
+                    "msg": str(e),
+                    "input": str(date_of_birth)
+                }]
+            )
+
+    @field_validator("info")
+    @classmethod
+    def validate_info_field(cls, info: str) -> str:
+        date_from_info = info.strip()
+        if not date_from_info:
+            raise HTTPException(
+                status_code=422,
+                detail=[{
+                    "type": "value_error",
+                    "loc": ["info"],
+                    "msg": "Info field cannot be empty or contain only spaces.",
+                    "input": info
+                }]
+            )
+        return date_from_info
+
+
+class ProfileResponseSchema(BaseModel):
+    id: int
+    first_name: str
+    last_name: str
+    gender: str
+    date_of_birth: date
+    info: str
+    avatar: HttpUrl
